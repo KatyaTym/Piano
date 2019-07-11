1d2779922c8ef3794654ee6d33bc1d86ef2df71b
Katya Tymkevych <palengakatusa01@gmail.com>
2019-07-11 13:41:26 +0300

    Update piano.js

--- piano.js
+++ piano.js
@@ -5,28 +5,28 @@ const sound = new Audio();
 function playAudio(key) {
     switch (key) {
         case 'a':
-            sound.src = "piano/1.mp3";
+            sound.src = "piano_music/1.mp3";
             break;
         case 's':
-            sound.src = "piano/2.mp3";
+            sound.src = "piano_music/2.mp3";
             break;
         case 'd':
-            sound.src = "piano/3.mp3";
+            sound.src = "piano_music/3.mp3";
             break;
         case 'f':
-            sound.src = "piano/4.mp3";
+            sound.src = "piano_music/4.mp3";
             break;
         case 'g':
-            sound.src = "piano/5.mp3";
+            sound.src = "piano_music/5.mp3";
             break;
         case 'h':
-            sound.src = "piano/6.mp3";
+            sound.src = "piano_music/6.mp3";
             break;
         case 'j':
-            sound.src = "piano/7.mp3";
+            sound.src = "piano_music/7.mp3";
             break;
         case 'k':
-            sound.src = "piano/8.mp3";
+            sound.src = "piano_music/8.mp3";
             break;
     }
     sound.play();
