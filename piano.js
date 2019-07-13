82888fbff05a9b1e8d5e86f4fd4b1bba078b620c
KatyaTym <palengakatusa01@gmail.com>
2019-07-13 12:30:21 +0300

    Optimize audio creation, misc fixes

--- piano.js
+++ piano.js
@@ -1,46 +1,31 @@
-var x = document.getElementById("myAudio");
+var soundsMap = new Map();
 
-const sound = new Audio();
+function playAudio(keyName) {
+    var sound = soundsMap.get(keyName);
+    if (!sound) {
+        sound = new Audio();
+        sound.src = "piano_music/" + keyName + ".mp3";
+        console.log(sound.src + " Created");
 
-function playAudio(key) {
-    switch (key) {
-        case 'a':
-            sound.src = "piano_music/1.mp3";
-            break;
-        case 's':
-            sound.src = "piano_music/2.mp3";
-            break;
-        case 'd':
-            sound.src = "piano_music/3.mp3";
-            break;
-        case 'f':
-            sound.src = "piano_music/4.mp3";
-            break;
-        case 'g':
-            sound.src = "piano_music/5.mp3";
-            break;
-        case 'h':
-            sound.src = "piano_music/6.mp3";
-            break;
-        case 'j':
-            sound.src = "piano_music/7.mp3";
-            break;
-        case 'k':
-            sound.src = "piano_music/8.mp3";
-            break;
+        soundsMap.set(keyName, sound);
     }
+    sound.currentTime = 0.0;
     sound.play();
 }
 
 function animateAndPlay(keyName) {
     const keyElement = document.getElementById(keyName + '_key');
-    const animationTimeout = 250;
 
-    keyElement.classList.add("play");
-    setTimeout(function() {
-        keyElement.classList.remove("play");
-    }, animationTimeout);
-    playAudio(keyName);
+    if (keyElement) {
+        const animationTimeout = 250;
+
+        keyElement.classList.add("play");
+        setTimeout(function() {
+            keyElement.classList.remove("play");
+        }, animationTimeout);
+
+        playAudio(keyName);
+    }
 }
 
 function addGlobalKeyPressEvent() {
