8402e69a5a79baa82c26611916c39813d53a9e84
KatyaTym <palengakatusa01@gmail.com>
2019-07-13 11:10:15 +0300

    Add key press handler

--- piano.js
+++ piano.js
@@ -31,3 +31,34 @@ function playAudio(key) {
     }
     sound.play();
 }
+
+document.addEventListener('keypress', (event) => {
+    const keyName = event.key;
+    switch (keyName) {
+        case 'a':
+            sound.src = "piano_music/1.mp3";
+            break;
+        case 's':
+            sound.src = "piano_music/2.mp3";
+            break;
+        case 'd':
+            sound.src = "piano_music/3.mp3";
+            break;
+        case 'f':
+            sound.src = "piano_music/4.mp3";
+            break;
+        case 'g':
+            sound.src = "piano_music/5.mp3";
+            break;
+        case 'h':
+            sound.src = "piano_music/6.mp3";
+            break;
+        case 'j':
+            sound.src = "piano_music/7.mp3";
+            break;
+        case 'k':
+            sound.src = "piano_music/8.mp3";
+            break;
+        }
+        sound.play();
+});
