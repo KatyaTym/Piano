b527c5b4611a2fa8b9bfee210a485bf7c6be2917
KatyaTym <palengakatusa01@gmail.com>
2019-07-13 11:17:38 +0300

    Animate and play moved to piano.js

--- piano.js
+++ piano.js
@@ -32,33 +32,22 @@ function playAudio(key) {
     sound.play();
 }
 
-document.addEventListener('keypress', (event) => {
-    const keyName = event.key;
-    switch (keyName) {
-        case 'a':
-            sound.src = "piano_music/1.mp3";
-            break;
-        case 's':
-            sound.src = "piano_music/2.mp3";
-            break;
-        case 'd':
-            sound.src = "piano_music/3.mp3";
-            break;
-        case 'f':
-            sound.src = "piano_music/4.mp3";
-            break;
-        case 'g':
-            sound.src = "piano_music/5.mp3";
-            break;
-        case 'h':
-            sound.src = "piano_music/6.mp3";
-            break;
-        case 'j':
-            sound.src = "piano_music/7.mp3";
-            break;
-        case 'k':
-            sound.src = "piano_music/8.mp3";
-            break;
-        }
-        sound.play();
-});
+function animateAndPlay(keyName) {
+    const keyElement = document.getElementById(keyName + '_key');
+    const animationTimeout = 250;
+
+    keyElement.classList.add("play");
+    setTimeout(function() {
+        keyElement.classList.remove("play");
+    }, animationTimeout);
+    playAudio(keyName);
+}
+
+function addGlobalKeyPressEvent() {
+    document.addEventListener('keypress', (event) => {
+        const keyName = event.key;
+        animateAndPlay(keyName);
+    });
+}
+
+addGlobalKeyPressEvent();
